cc15aa41a417338808bd8b84b8ec5039f711ca0a
Albert Kuc <albert.kuc@mssint.com>
2022-03-30 09:25:00 +0100

    Add game history

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,35 +1,8 @@
 import React from "react";
 import Square from './Square'
 import "./Square.css"
-import calculateWinner from "./calculateWinner";
 
 class Board extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      squares: Array(9).fill(null),
-      xIsNext: true,
-    }
-  }
-
-  /**
-   * Assigns all square values to a new array
-   * Ignores click if there is already a winner declared, or the field is already occupied.
-   * Otherwise, changes status (symbol) of the specified square in the new array.
-   * Changes state of squares and xIsNext.
-   * @param i: number
-   */
-  handleClick = i => {
-    const newSquares = this.state.squares.slice();
-    if (calculateWinner(newSquares) || newSquares[i]) {
-      return;
-    }
-    newSquares[i] = this.state.xIsNext ? 'X': 'O';
-    this.setState({
-      squares: newSquares,
-      xIsNext: !this.state.xIsNext,
-    })
-  }
 
   /**
    * Renders individual square with value from specified square
@@ -39,29 +12,19 @@ class Board extends React.Component {
   renderSquare = i => {
     return (
       <Square
-        value={this.state.squares[i]}
-        onClick={() => this.handleClick(i)}
+        value={this.props.squares[i]}
+        onClick={() => this.props.onClick(i)}
       />
     );
   }
 
   /**
-   * Checks winner status and updates nextTurnStatus.
    * Returns 3x3 Square components
    * @returns {JSX.Element}
    */
   render() {
-    const winner = calculateWinner(this.state.squares);
-    let nextTurnStatus;
-    if (winner) {
-      nextTurnStatus = 'Winner ' + winner;
-    } else {
-      nextTurnStatus = 'Next player: ' + (this.state.xIsNext ? 'X': 'O');
-    }
-
     return (
       <div>
-        <div className="status">{nextTurnStatus}</div>
         <div className="board-row">
           {this.renderSquare(0)}
           {this.renderSquare(1)}
--- src/components/Game.js
+++ src/components/Game.js
@@ -1,16 +1,63 @@
 import React from "react";
 import Board from "./Board";
 import "./Board.css"
+import calculateWinner from "./calculateWinner";
 
 class Game extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      history: [{
+        squares: Array(9).fill(null),
+      }],
+      xIsNext: true,
+    }
+  }
+
+  /**
+   * Assigns all square values to a new array
+   * Ignores click if there is already a winner declared, or the field is already occupied.
+   * Otherwise, changes status (symbol) of the specified square in the new array.
+   * Changes state of squares and xIsNext.
+   * @param i: number
+   */
+  handleClick = i => {
+    const history = this.state.history;
+    const current = history[history.length - 1];
+    const newSquares = current.squares.slice();
+    if (calculateWinner(newSquares) || newSquares[i]) {
+      return;
+    }
+    newSquares[i] = this.state.xIsNext ? 'X': 'O';
+    this.setState({
+      history: history.concat([{
+        squares: newSquares
+      }]),
+      xIsNext: !this.state.xIsNext,
+    });
+  }
+
   render() {
+    const history = this.state.history;
+    const current = history[history.length - 1];
+    const winner = calculateWinner(current.squares);
+
+    let status;
+    if (winner) {
+      status = 'Winner: ' + winner;
+    } else {
+      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    }
     return (
       <div className="game">
         <div className="game-board">
-          <Board />
+          <Board
+            squares={current.squares}
+            onClick={(i) => this.handleClick(i)}
+          />
         </div>
         <div className="game-info">
-          <div>{/* status */}</div>
+          <div>{status}</div>
           <ol>{/* TODO */}</ol>
         </div>
       </div>
