8ccc3a783500b023fa124548d7503c8aac5da980
Albert Kuc <albert.kuc@mssint.com>
2022-03-30 09:45:17 +0100

    Add buttons to undo/repeat moves

--- src/components/Game.js
+++ src/components/Game.js
@@ -10,6 +10,7 @@ class Game extends React.Component {
       history: [{
         squares: Array(9).fill(null),
       }],
+      stepNumber: 0,
       xIsNext: true,
     }
   }
@@ -22,7 +23,8 @@ class Game extends React.Component {
    * @param i: number
    */
   handleClick = i => {
-    const history = this.state.history;
+    /* Slice ensures that if we go back in time, and make a new move, we throw away all the incorrect history */
+    const history = this.state.history.slice(0, this.state.stepNumber + 1);
     const current = history[history.length - 1];
     const newSquares = current.squares.slice();
     if (calculateWinner(newSquares) || newSquares[i]) {
@@ -33,14 +35,39 @@ class Game extends React.Component {
       history: history.concat([{
         squares: newSquares
       }]),
+      stepNumber: history.length,
       xIsNext: !this.state.xIsNext,
+    });
+  }
+
+  /**
+   * Updates stepNumber
+   * Sets xIsNext to true if stepNumber changes to even
+   * @param step
+   */
+  jumpTo(step) {
+    this.setState({
+      stepNumber: step,
+      xIsNext: (step % 2) === 0,
     });
   }
 
   render() {
     const history = this.state.history;
-    const current = history[history.length - 1];
+    const current = history[this.state.stepNumber];
     const winner = calculateWinner(current.squares);
+
+    /* Step variable refers to the current history element and move refers to the current history element idx */
+    const moves = history.map((step, move) => {
+      const desc = move ?
+        'Go to move #' + move :
+        'Go to game start';
+      return (
+        <li key={move}>
+          <button onClick={() => this.jumpTo(move)}>{desc}</button>
+        </li>
+      );
+    });
 
     let status;
     if (winner) {
@@ -58,7 +85,7 @@ class Game extends React.Component {
         </div>
         <div className="game-info">
           <div>{status}</div>
-          <ol>{/* TODO */}</ol>
+          <ol>{moves}</ol>
         </div>
       </div>
     );
